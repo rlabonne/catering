f3bbff9552fea0253f3879476077b1151224615c
Rob LaBonne <roblabonne@gmail.com>
2018-03-04 17:34:05 -0500

    contact views updated

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,16 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
   def new
   end
 
   def edit
+    @contact = Contact.find(params[:id])
   end
 end
