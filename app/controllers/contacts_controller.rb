071348a717f614e47b78172538edb48a713f7aa7
Rob LaBonne <roblabonne@gmail.com>
2018-03-18 23:00:30 -0400

    contact model complete

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   def index
-    @contacts = Contact.all.order(:lastName)
+    @contacts = Contact.all.order(:firstName)
   end
 
   def show
@@ -8,6 +8,32 @@ class ContactsController < ApplicationController
   end
 
   def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new
+    @contact.firstName = params[:contact][:firstName]
+    @contact.lastName = params[:contact][:lastName]
+    @contact.company = params[:contact][:company]
+    @contact.phone1 = params[:contact][:phone1]
+    @contact.phone2 = params[:contact][:phone2]
+    @contact.email = params[:contact][:email]
+    @contact.address1 = params[:contact][:address1]
+    @contact.address2 = params[:contact][:address2]
+    @contact.city = params[:contact][:city]
+    @contact.state = params[:contact][:state]
+    @contact.zipcode = params[:contact][:zipcode]
+    @contact.notes = params[:contact][:notes]
+    @contact.user = current_user
+
+    if @contact.save
+      flash[:notice] = "Contact was saved."
+      redirect_to @contact
+    else
+      flash.now[:alert] = "There was an error saving the contact. Please try again."
+      render :new
+    end
   end
 
   def edit
@@ -35,6 +61,18 @@ class ContactsController < ApplicationController
     else
       flash.now[:alert] = "There was an error saving the contact. Please try again."
       render :edit
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+
+    if @contact.destroy
+      flash[:notice] = "\"#{@contact.firstName} #{@contact.lastName}\" was successfully deleted."
+      redirect_to contacts_path
+    else
+      flash.now[:alert] = "There was an error deleting the contact. Please try again."
+      render :show
     end
   end
 end
